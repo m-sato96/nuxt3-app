9e8b20d81600e700a06f96746eb8c44d32b5022c
m-sato <ssmmaagg36@gmail.com>
2023-10-15 12:36:21 +0900

    リンク追加

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,8 @@
   <div>
     <h1>トップページ</h1>
     <hr />
+    <NuxtLink to="/price">料金ページへ</NuxtLink>
+    <br />
+    <NuxtLink to="https://www.google.co.jp/">Google</NuxtLink>
   </div>
 </template>
--- pages/price.vue
+++ pages/price.vue
@@ -3,5 +3,6 @@
   <div>
     <h1>料金ページ</h1>
     <hr />
+    <NuxtLink to="/">トップページへ</NuxtLink>
   </div>
 </template>
